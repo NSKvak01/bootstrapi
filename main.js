2f5a990b0778ff7610f9e112bee170df2aaf375e
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-04-08 10:31:58 -0400

    Final

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Part 1
+
 const dogButton = document.querySelector('#dog-button')
 const dogImage = document.querySelector('#dog-image')
 const URL = 'https://dog.ceo/api/breeds/image/random'
@@ -9,4 +11,89 @@ dogButton.addEventListener('click', ()=>{
             dogImage.src = response.message
         })
         .catch((error)=> console.log(error))
+})
+
+
+
+
+// Part 2
+
+const weatherButton = document.querySelector('#weather-button')
+const temperature = document.querySelector('#temperature')
+const wind = document.querySelector('#wind')
+const description  = document.querySelector('#description')
+
+weatherButton.addEventListener('click', ()=>{
+    const input = document.querySelector('#weather')
+    inputValue = input.value
+    // inputValue.replace()
+    const URL1 = "https://goweather.herokuapp.com/weather/"+inputValue
+    fetch(URL1)
+        .then ((data)=>data.json())
+        .then ((response)=>{
+            temperature.innerText = response.temperature
+            wind.innerText = response.wind
+            description.innerText = response.description
+            const body = document.querySelector('body')
+            if (response.description ==='Light rain'){
+                body.style.backgroundImage = "url('images/62OPjF.jpeg')" 
+                body.style.backgroundSize = '100%'
+            } else if (response.description ==='Sunny'){
+                body.style.backgroundImage = "url('images/unnamed.jpeg')" 
+                body.style.backgroundSize = '100%'
+            } else if (response.description ==='Clear'){
+                body.style.backgroundImage = "url('images/clear.jpeg')" 
+                body.style.backgroundSize = '100%'
+            } else if (response.description ==='Partly cloudy'){
+                body.style.backgroundImage = "url('images/cloudy.jpeg')" 
+                body.style.backgroundSize = '100%'
+            } else if (response.description ==='Patchy rain possible'){
+                body.style.backgroundImage = "url('images/patchy.jpeg')" 
+                body.style.backgroundSize = '100%'
+            } else if (response.description ==='Light snow'){
+                body.style.backgroundImage = "url('images/lightsnow.jpeg')" 
+                body.style.backgroundSize = '100%'
+            } else if (response.description ==='Moderate rain at times'){
+                body.style.backgroundImage = "url('images/moderaterain.jpeg')" 
+                body.style.backgroundSize = '100%'
+            }
+        })
+})
+
+document.addEventListener('keydown', (e)=>{
+    if (e.key === 'Enter'){
+        const input = document.querySelector('#weather')
+        inputValue = input.value
+        const URL1 = "https://goweather.herokuapp.com/weather/"+inputValue
+        fetch(URL1)
+            .then ((data)=>data.json())
+            .then ((response)=>{
+                temperature.innerText = response.temperature
+                wind.innerText = response.wind
+                description.innerText = response.description
+                const body = document.querySelector('body')
+                if (response.description ==='Light rain'){
+                    body.style.backgroundImage = "url('images/62OPjF.jpeg')" 
+                    body.style.backgroundSize = '100%'
+                } else if (response.description ==='Sunny'){
+                    body.style.backgroundImage = "url('images/unnamed.jpeg')" 
+                    body.style.backgroundSize = '100%'
+                } else if (response.description ==='Clear'){
+                    body.style.backgroundImage = "url('images/clear.jpeg')" 
+                    body.style.backgroundSize = '100%'
+                } else if (response.description ==='Partly cloudy'){
+                    body.style.backgroundImage = "url('images/cloudy.jpeg')" 
+                    body.style.backgroundSize = '100%'
+                } else if (response.description ==='Patchy rain possible'){
+                    body.style.backgroundImage = "url('images/patchy.jpeg')" 
+                    body.style.backgroundSize = '100%'
+                } else if (response.description ==='Light snow'){
+                    body.style.backgroundImage = "url('images/lightsnow.jpeg')" 
+                    body.style.backgroundSize = '100%'
+                } else if (response.description ==='Moderate rain at times'){
+                    body.style.backgroundImage = "url('images/moderaterain.jpeg')" 
+                    body.style.backgroundSize = '100%'
+                }
+            })
+    }
 })
